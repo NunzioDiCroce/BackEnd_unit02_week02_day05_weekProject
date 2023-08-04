280fa947a41e62f4c55558a55b1f3ef141068ff6
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-04 10:48:11 +0200

    Update Utente.java

--- GestioneDispositivi/src/main/java/com/example/GestioneDispositivi/entities/Utente.java
+++ GestioneDispositivi/src/main/java/com/example/GestioneDispositivi/entities/Utente.java
@@ -1,5 +1,30 @@
 package com.example.GestioneDispositivi.entities;
 
+import java.util.Set;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
 public class Utente {
+
+	@Id
+	@GeneratedValue
+	private long id;
+
+	protected String userName;
+	protected String nome;
+	protected String cognome;
+	protected String mail;
+
+	@OneToMany
+	protected Set<Dispositivo> dispositivi;
+
+	public Utente(String userName, String nome, String cognome, String mail) {
+		this.userName = userName;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.mail = mail;
+	}
 
 }
