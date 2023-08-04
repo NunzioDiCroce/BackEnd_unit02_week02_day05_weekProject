cace8ec581fbdead3eee3e518ef8e64d83aa8684
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-04 10:58:53 +0200

    Create TipoDispositivo.java

---
+++ GestioneDispositivi/src/main/java/com/example/GestioneDispositivi/enums/StatoDispositivo.java
@@ -0,0 +1,5 @@
+package com.example.GestioneDispositivi.enums;
+
+public enum StatoDispositivo {
+
+}
---
+++ GestioneDispositivi/src/main/java/com/example/GestioneDispositivi/enums/TipoDispositivo.java
@@ -0,0 +1,6 @@
+package com.example.GestioneDispositivi.enums;
+
+public enum TipoDispositivo {
+	SMARTPHONE, TABLET, LAPTOP
+
+}
