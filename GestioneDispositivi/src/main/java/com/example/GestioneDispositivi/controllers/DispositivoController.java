562774aa3b603739d22e49c246168d8e30cee8f7
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-04 12:02:34 +0200

    Update DispositivoController.java

--- GestioneDispositivi/src/main/java/com/example/GestioneDispositivi/controllers/DispositivoController.java
+++ GestioneDispositivi/src/main/java/com/example/GestioneDispositivi/controllers/DispositivoController.java
@@ -7,11 +7,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.GestioneDispositivi.entities.Dispositivo;
+import com.example.GestioneDispositivi.entities.DispositivoPayload;
 import com.example.GestioneDispositivi.services.DispositivoService;
 
 @RestController
@@ -25,12 +29,12 @@ public class DispositivoController {
 		this.dispositivoService = dispositivoService;
 	}
 
-//	@PostMapping
-//	@ResponseStatus(HttpStatus.CREATED)
-//	public Dispositivo saveDevice(@RequestBody DispositivoPayload body) {
-//		Dispositivo created = dispositivoService.save(body);
-//		return created;
-//	}
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Dispositivo saveDevice(@RequestBody DispositivoPayload body) {
+		Dispositivo created = dispositivoService.save(body);
+		return created;
+	}
 
 	@GetMapping("")
 	public List<Dispositivo> getDevices() {
@@ -43,10 +47,10 @@ public class DispositivoController {
 
 	}
 
-//	@PutMapping("/{deviceId}")
-//	public Dispositivo updateDevice(@PathVariable long deviceId, @RequestBody DispositivoPayload body) {
-//		return dispositivoService.findByIdAndUpdate(deviceId, body);
-//	}
+	@PutMapping("/{deviceId}")
+	public Dispositivo updateDevice(@PathVariable long deviceId, @RequestBody DispositivoPayload body) {
+		return dispositivoService.findByIdAndUpdate(deviceId, body);
+	}
 
 	@DeleteMapping("/{deviceId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
