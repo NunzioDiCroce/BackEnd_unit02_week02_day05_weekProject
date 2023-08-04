cf36d8a42df13620018d0a3427bca9b7ea65b80d
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-04 10:56:42 +0200

    Update Dispositivo.java

--- GestioneDispositivi/src/main/java/com/example/GestioneDispositivi/entities/Dispositivo.java
+++ GestioneDispositivi/src/main/java/com/example/GestioneDispositivi/entities/Dispositivo.java
@@ -1,5 +1,47 @@
 package com.example.GestioneDispositivi.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Entity
+@Table(name = "dispositivi")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Slf4j
+@Builder
 public class Dispositivo {
+
+	@Id
+	@GeneratedValue
+	private long id;
+
+	@Enumerated(EnumType.STRING)
+	protected TipoDispositivo tipoDispositivo;
+
+	@Enumerated(EnumType.STRING)
+	protected StatoDispositivo statoDispositivo;
+
+	@ManyToOne
+	protected Utente utente;
+
+	public Dispositivo(TipoDispositivo tipoDispositivo, StatoDispositivo statoDispositivo) {
+		this.tipoDispositivo = tipoDispositivo;
+		this.statoDispositivo = statoDispositivo;
+	}
 
 }
