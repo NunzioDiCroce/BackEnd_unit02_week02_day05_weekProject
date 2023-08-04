634515250b8de23e70173c262ee67785f1c923c7
NunzioDiCroce <nunziodavide@gmail.com>
2023-08-04 10:50:14 +0200

    Update Utente.java

--- GestioneDispositivi/src/main/java/com/example/GestioneDispositivi/entities/Utente.java
+++ GestioneDispositivi/src/main/java/com/example/GestioneDispositivi/entities/Utente.java
@@ -2,10 +2,28 @@ package com.example.GestioneDispositivi.entities;
 
 import java.util.Set;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
+@Entity
+@Table(name = "utenti")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Slf4j
+@Builder
 public class Utente {
 
 	@Id
